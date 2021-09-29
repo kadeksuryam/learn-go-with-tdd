f63ce96af2aff21514aea522e375fcc7619c1152
kadeksuryam <kadeksuryam@gmail.com>
2021-09-29 22:19:30 +0700

    refactor(iteration): repeat const

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,11 @@
 package iteration
 
+const nRep = 5
+
 func Repeat(s string) string {
 	var repStr string
-	for i := 0; i < 5; i++ {
-		repStr = repStr + "a"
+	for i := 0; i < nRep; i++ {
+		repStr += "a"
 	}
 	return repStr
 }
